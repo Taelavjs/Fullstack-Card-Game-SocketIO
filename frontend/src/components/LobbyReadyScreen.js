a57d0ff8b071fb4202ac01ab6722cf524da41b1a
TaelaVJS <taelavjs@hotmail.com>
2024-06-20 12:59:38 +0100

    refactored match lobby page
    seperated functionality into new file

---
+++ frontend/src/components/LobbyReadyScreen.js
@@ -0,0 +1,30 @@
+
+
+const LobbyReadyScreen = ({readyUp, match}) => {
+
+
+    return (
+        <>
+            <div className="relative h-screen w-full bg-gradient-to-b from-pastel-purple to-pastel-blue overflow-hidden">
+            <div className="absolute top-10 left-1/2 transform -translate-x-1/2 text-center text-3xl font-bold text-white">
+                Players
+            </div>
+            <div className="absolute top-1/3 left-1/2 transform -translate-x-1/2 flex flex-col space-y-4 items-center">
+                {match.map((playerName, index) => (
+                <div key={index} className="bg-pastel-pink text-white px-6 py-3 rounded-none shadow-lg">
+                    {playerName}
+                </div>
+                ))}
+                <button
+                onClick={readyUp}
+                className="bg-pastel-purple text-white px-6 py-3 rounded-none hover:bg-pastel-purple-dark transition duration-300 shadow-lg transform hover:-translate-y-1 hover:scale-105"
+                >
+                Ready?
+                </button>
+            </div>
+            </div>
+        </>
+    )
+}
+
+export default LobbyReadyScreen;
--- frontend/src/components/matchMaking.js
+++ frontend/src/components/matchMaking.js
@@ -6,6 +6,7 @@ import LobbySelector from './lobbySelector';
 import { createContext, useContext } from "react";
 import { UserContext } from '../App';
 import LobbyInput from './LobbyInput';
+import LobbyReadyScreen from './LobbyReadyScreen';
 
 const Matchmaking = () => {
 
@@ -95,24 +96,7 @@ const Matchmaking = () => {
   )}
 
   {match && match.length > 0 && !gameStart && (
-    <div className="relative h-screen w-full bg-gradient-to-b from-pastel-purple to-pastel-blue overflow-hidden">
-      <div className="absolute top-10 left-1/2 transform -translate-x-1/2 text-center text-3xl font-bold text-white">
-        Players
-      </div>
-      <div className="absolute top-1/3 left-1/2 transform -translate-x-1/2 flex flex-col space-y-4 items-center">
-        {match.map((playerName, index) => (
-          <div key={index} className="bg-pastel-pink text-white px-6 py-3 rounded-none shadow-lg">
-            {playerName}
-          </div>
-        ))}
-        <button
-          onClick={readyUp}
-          className="bg-pastel-purple text-white px-6 py-3 rounded-none hover:bg-pastel-purple-dark transition duration-300 shadow-lg transform hover:-translate-y-1 hover:scale-105"
-        >
-          Ready?
-        </button>
-      </div>
-    </div>
+    <LobbyReadyScreen readyUp = {readyUp} match={match}/>
   )}
 
   {match && gameStart && winner === "" && (
